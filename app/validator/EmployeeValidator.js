990ba3b31ddea5d1b2129a8f29ec7e9cd3aa780b
shaunganley <s.ganley@kainos.com>
2023-10-31 12:11:01 +0000

    fix constructor error

--- app/validator/EmployeeValidator.js
+++ app/validator/EmployeeValidator.js
@@ -1,15 +1,17 @@
-module.exports.validateEmployee = function (employee) {
-    if (isNaN(employee.salary)) {
-        return "Salary must be a number"
-    }
+module.exports = class EmployeeValidator {
+    validateEmployee = function (employee) {
+        if (isNaN(employee.salary)) {
+            return "Salary must be a number"
+        }
 
-    if (Number(employee.salary) < 20000) {
-        return "Salary must be at least £20,000"
-    }
+        if (Number(employee.salary) < 20000) {
+            return "Salary must be at least £20,000"
+        }
 
-    if (employee.bankNo.length != 8) {
-        return "Invalid bank number"
-    }
+        if (employee.bankNo.length != 8) {
+            return "Invalid bank number"
+        }
 
-    return null
+        return null
+    }
 }
