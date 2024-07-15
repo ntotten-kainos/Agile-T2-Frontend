1816a6f766a800d699d3171fcc3641f1916066f9
ntotten-kainos <nathan.totten@kainos.com>
2024-07-15 10:25:50 +0100

    Test exercise and refactor

--- src/validators/EmployeeValidator.ts
+++ src/validators/EmployeeValidator.ts
@@ -8,4 +8,16 @@ export const validateEmployeeRequest = function (employeeRequest: EmployeeReques
     if (employeeRequest.bankNo.length != 8) {
         throw new Error("Invalid bank number");
     }
+
+    if (employeeRequest.fname.length > 50) {
+        throw new Error("Forename must be 50 characters or less");
+    }
+
+    if (employeeRequest.lname.length > 50) {
+        throw new Error("Surname must be 50 characters or less");
+    }
+
+    if (employeeRequest.nin.length != 9) {
+        throw new Error("National Insurance Number must be 9 characters");
+    }
 }
--- test/unit/validators/EmployeeValidatorTests.ts
+++ test/unit/validators/EmployeeValidatorTests.ts
@@ -4,56 +4,56 @@ import { EmployeeRequest } from '../../../src/models/EmployeeRequest'
 
 describe('EmployeeValidator', function () {
     describe('validateEmployeeRequest', function () {
-      it('should not throw exception when errors', () => {
-        const employeeRequest: EmployeeRequest = {
-            salary: 30000,
-            fname: "Mocha",
-            lname: "Chai",
-            email: "test@email.com",
-            address: "address",
-            address2: "address2",
-            city: "city",
-            county: "county",
-            postalCode: "postalCode",
-            country: "country",
-            phoneNo: "01234567890",
-            bankNo: "12345678",
-            nin: "12345678"
-        }
+        it('should not throw exception when errors', () => {
+          const employeeRequest: EmployeeRequest = {
+              salary: 30000,
+              fname: "Mocha",
+              lname: "Chai",
+              email: "test@email.com",
+              address: "address",
+              address2: "address2",
+              city: "city",
+              county: "county",
+              postalCode: "postalCode",
+              country: "country",
+              phoneNo: "01234567890",
+              bankNo: "12345678",
+              nin: "123456789"
+          }
 
-        try {
-          validateEmployeeRequest(employeeRequest);
-        } catch (e) {
-          assert.fail("Expected no error message");
-        } 
-      })
+          try {
+            validateEmployeeRequest(employeeRequest);
+          } catch (e) {
+            assert.fail("Expected no error message");
+          } 
+        })
 
-      it('should return error when salary too low', () => {
-        const employeeRequest: EmployeeRequest = {
-            salary: 10000,
-            fname: "Mocha",
-            lname: "Chai",
-            email: "test@email.com",
-            address: "address",
-            address2: "address2",
-            city: "city",
-            county: "county",
-            postalCode: "postalCode",
-            country: "country",
-            phoneNo: "01234567890",
-            bankNo: "12345678",
-            nin: "12345678"
-        }
+        it('should return error when salary too low', () => {
+          const employeeRequest: EmployeeRequest = {
+              salary: 10000,
+              fname: "Mocha",
+              lname: "Chai",
+              email: "test@email.com",
+              address: "address",
+              address2: "address2",
+              city: "city",
+              county: "county",
+              postalCode: "postalCode",
+              country: "country",
+              phoneNo: "01234567890",
+              bankNo: "12345678",
+              nin: "123456789"
+          }
 
-        try {
-          validateEmployeeRequest(employeeRequest);
-        } catch (e) {
-          expect(e.message).to.equal("Salary must be at least £20,000");
-          return;
-        }
+          try {
+            validateEmployeeRequest(employeeRequest);
+          } catch (e) {
+            expect(e.message).to.equal("Salary must be at least £20,000");
+            return;
+          }
 
-        assert.fail("Expected error message");
-      })
+          assert.fail("Expected error message");
+        })
 
     /*
     Unit Test Exercise 1
@@ -67,6 +67,33 @@ describe('EmployeeValidator', function () {
     This should pass without code changes
      */
 
+      it('should return error when bank number is too short', () => {
+        const employeeRequest: EmployeeRequest = {
+            salary: 20000,
+            fname: "Mocha",
+            lname: "Chai",
+            email: "test@email.com",
+            address: "address",
+            address2: "address2",
+            city: "city",
+            county: "county",
+            postalCode: "postalCode",
+            country: "country",
+            phoneNo: "01234567890",
+            bankNo: "123456",
+            nin: "123456789"
+        }
+
+        try {
+          validateEmployeeRequest(employeeRequest);
+        } catch (e) {
+          expect(e.message).to.equal("Invalid bank number");
+          return;
+        }
+
+        assert.fail("Expected error message");
+      })
+
     /*
     Unit Test Exercise 2
 
@@ -78,6 +105,32 @@ describe('EmployeeValidator', function () {
 
     This should pass without code changes
      */
+      it('should return error when bank number is too long', () => {
+        const employeeRequest: EmployeeRequest = {
+            salary: 20000,
+            fname: "Mocha",
+            lname: "Chai",
+            email: "test@email.com",
+            address: "address",
+            address2: "address2",
+            city: "city",
+            county: "county",
+            postalCode: "postalCode",
+            country: "country",
+            phoneNo: "01234567890",
+            bankNo: "123456789",
+            nin: "123456789"
+        }
+
+        try {
+          validateEmployeeRequest(employeeRequest);
+        } catch (e) {
+          expect(e.message).to.equal("Invalid bank number");
+          return;
+        }
+
+        assert.fail("Expected error message");
+      })
 
     /*
     Unit Test Exercise 3
@@ -91,6 +144,33 @@ describe('EmployeeValidator', function () {
     This should fail, make code changes to make this test pass
      */
 
+      it('should return error when forename is too long', () => {
+        const employeeRequest: EmployeeRequest = {
+            salary: 20000,
+            fname: "MochaMochaMochaMochaMochaMochaMochaMochaMochaMochaMochaMochaMochaMocha",
+            lname: "Chai",
+            email: "test@email.com",
+            address: "address",
+            address2: "address2",
+            city: "city",
+            county: "county",
+            postalCode: "postalCode",
+            country: "country",
+            phoneNo: "01234567890",
+            bankNo: "12345678",
+            nin: "123456789"
+        }
+
+        try {
+          validateEmployeeRequest(employeeRequest);
+        } catch (e) {
+          expect(e.message).to.equal("Forename must be 50 characters or less");
+          return;
+        }
+
+        assert.fail("Expected error message");
+      })
+
     /*
     Unit Test Exercise 4
 
@@ -102,6 +182,32 @@ describe('EmployeeValidator', function () {
 
     This should fail, make code changes to make this test pass
      */
+      it('should return error when forename is too long', () => {
+        const employeeRequest: EmployeeRequest = {
+            salary: 20000,
+            fname: "Mocha",
+            lname: "ChaiChaiChaiChaiChaiChaiChaiChaiChaiChaiChaiChaiChaiChaiChaiChaiChaiChaiChaiChaiChaiChai",
+            email: "test@email.com",
+            address: "address",
+            address2: "address2",
+            city: "city",
+            county: "county",
+            postalCode: "postalCode",
+            country: "country",
+            phoneNo: "01234567890",
+            bankNo: "12345678",
+            nin: "123456789"
+        }
+
+        try {
+          validateEmployeeRequest(employeeRequest);
+        } catch (e) {
+          expect(e.message).to.equal("Surname must be 50 characters or less");
+          return;
+        }
+
+        assert.fail("Expected error message");
+      })
 
     /*
     Unit Test Exercise 5
@@ -114,6 +220,32 @@ describe('EmployeeValidator', function () {
 
     This should fail, make code changes to make this test pass
      */
+      it('should return error when nin is too long', () => {
+        const employeeRequest: EmployeeRequest = {
+            salary: 20000,
+            fname: "Mocha",
+            lname: "Chai",
+            email: "test@email.com",
+            address: "address",
+            address2: "address2",
+            city: "city",
+            county: "county",
+            postalCode: "postalCode",
+            country: "country",
+            phoneNo: "01234567890",
+            bankNo: "12345678",
+            nin: "123456789104"
+        }
+
+        try {
+          validateEmployeeRequest(employeeRequest);
+        } catch (e) {
+          expect(e.message).to.equal("National Insurance Number must be 9 characters");
+          return;
+        }
+
+        assert.fail("Expected error message");
+      })
 
     /*
     Unit Test Exercise 6
@@ -126,5 +258,31 @@ describe('EmployeeValidator', function () {
 
     This should fail, make code changes to make this test pass
      */
+      it('should return error when nin is too short', () => {
+        const employeeRequest: EmployeeRequest = {
+            salary: 20000,
+            fname: "Mocha",
+            lname: "Chai",
+            email: "test@email.com",
+            address: "address",
+            address2: "address2",
+            city: "city",
+            county: "county",
+            postalCode: "postalCode",
+            country: "country",
+            phoneNo: "01234567890",
+            bankNo: "12345678",
+            nin: "1234567"
+        }
+
+        try {
+          validateEmployeeRequest(employeeRequest);
+        } catch (e) {
+          expect(e.message).to.equal("National Insurance Number must be 9 characters");
+          return;
+        }
+
+        assert.fail("Expected error message");
+      })
     })
   })
