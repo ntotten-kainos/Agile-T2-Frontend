1d59f0ea88ed4db07e0c8909b973a5c381d1548b
mark-mcilroykainos <mark.mcilroy@kainos.com>
2024-07-22 16:46:15 +0100

    modified app.ts to include /job-role endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
-import { getIndex, getEmployees, getEmployee, getEmployeeForm, postEmployeeForm } from "./controllers/EmployeeController";
 import express from "express";
 import nunjucks from "nunjucks";
-import bodyParser from "body-parser";
+
+import { getAllJobRoles } from "./controllers/JobRoleController";
+
 const app = express();
 
 nunjucks.configure('views', {
@@ -9,20 +10,13 @@ nunjucks.configure('views', {
     express: app
 });
 
-app.use(express.static('public'));
-app.set('view engine', 'html')
+//app.use(express.static('public'));
+//app.set('view engine', 'html')
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({
-  extended: true
-}))
+
 
 app.listen(3000, () => {
     console.log('Server started on port 3000');
 });
 
-app.get('/', getIndex);
-app.get('/employees', getEmployees);
-app.get('/employees/:id', getEmployee);
-app.get('/insert-employee', getEmployeeForm);
-app.post('/insert-employee', postEmployeeForm);
+app.get('/job-role', getAllJobRoles);
