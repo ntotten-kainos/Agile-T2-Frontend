77fc4eeb9c54c2ea542a77a36ee3172bd5ffafcf
mark-mcilroykainos <mark.mcilroy@kainos.com>
2024-07-22 15:47:41 +0100

    installed dependencies (axios) added nodemon/ts-node, created JobRoleResponse.ts, created JobRoleService.ts

---
+++ src/models/JobRoleResponse.ts
@@ -0,0 +1,10 @@
+export type JobRoleResponse = { 
+    jobRoleId: Number,
+    roleName: String,
+    location: String, //enum as string? String or string?
+    band: String,
+    capability: String,
+    closingDate: Date
+
+
+}
---
+++ src/services/JobRoleService.ts
@@ -0,0 +1,12 @@
+import axios, {AxiosResponse} from "axios";
+import { JobRoleResponse } from "../models/JobRoleResponse";
+
+export const getJobRoles = async (): Promise<JobRoleResponse[]> => {
+    try {
+        const response: AxiosResponse = await axios.get("http://localhost:8080/api/roles"); // will need amended
+        return response.data;
+    } catch(e) {
+        console.log(e);
+        throw new Error('Failed to get Job Roles');
+    }
+}
