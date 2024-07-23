1bf073b027af192fb26b8899ebd9ed452d096877
mark-mcilroykainos <mark.mcilroy@kainos.com>
2024-07-23 11:57:05 +0100

    fixed error in getAllJobRoles method, unit test now passing

--- src/controllers/JobRoleController.ts
+++ src/controllers/JobRoleController.ts
@@ -2,5 +2,11 @@ import express from "express";
 import { getJobRoles } from "../services/JobRoleService";
 
 export const getAllJobRoles = async (req: express.Request, res: express.Response): Promise<void> => {
-    res.render('jobRoles.html', { jobRoles: await getJobRoles() });
-}
+    try{
+        res.render('jobRoles', { jobRoles: await getJobRoles() });
+    } catch (error){
+        res.locals.errormessage = error.message;
+        res.render('jobRoles');
+    }
+
+    };
