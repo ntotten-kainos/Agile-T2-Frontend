280e17c4a27f62fceae568049d5717a106776cea
ntotten-kainos <nathan.totten@kainos.com>
2024-07-24 09:19:10 +0100

    Update package.json and app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import nunjucks from "nunjucks";
 import bodyParser from "body-parser";
+import session from "express-session";
 const app = express();
 
 nunjucks.configure('views', {
@@ -15,6 +16,14 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
   extended: true
 }))
+
+app.use(session({ secret: 'SUPER_DUPER_SECRET', cookie: { maxAge: 28800000 } }));
+
+declare module "express-session" {
+  interface SessionData {
+    token: string;
+  }
+}
 
 app.listen(3000, () => {
     console.log('Server started on port 3000');
