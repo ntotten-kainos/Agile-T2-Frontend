4aa953ebabd5613032c9a51320a2f6357c38c344
mark-mcilroykainos <mark.mcilroy@kainos.com>
2024-07-25 09:50:44 +0100

    passing the base URL in as an enviornent variable for deployment on AWS

--- src/services/JobRoleService.ts
+++ src/services/JobRoleService.ts
@@ -1,9 +1,14 @@
 import axios, { AxiosResponse } from "axios";
 import { JobRoleResponse } from "../models/JobRoleResponse";
 
+axios.defaults.baseURL = process.env.API_URL || 'http://localhost:8080/';
+
+export const URL: string = "/api/job-roles/";
+
+
 export const getJobRoles = async (): Promise<JobRoleResponse[]> => {
     try {
-        const response: AxiosResponse = await axios.get("http://localhost:8080/api/job-roles"); 
+        const response: AxiosResponse = await axios.get(URL); 
         return response.data;
     } catch (e) {
         console.log(e);
