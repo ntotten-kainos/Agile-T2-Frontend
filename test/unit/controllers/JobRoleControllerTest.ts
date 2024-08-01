0e022efddb0d604778d148cdebedf901701a8e3c
Jamie McConnell <jamie.mcconnell@kainos.com>
2024-08-01 12:52:33 +0100

    Unit tests added to JobRoleControllerTest.ts

--- test/unit/controllers/JobRoleControllerTest.ts
+++ test/unit/controllers/JobRoleControllerTest.ts
@@ -3,6 +3,7 @@ import * as JobRoleService from "../../../src/services/JobRoleService";
 import { JobRoleResponse } from "../../../src/models/JobRoleResponse";
 import { expect } from 'chai';
 import sinon from 'sinon';
+import { JobRoleDetailResponse } from "../../../src/models/JobRoleDetailResponse";
 
 const jobRoleResponse: JobRoleResponse = {
     jobRoleId: 1,
@@ -12,6 +13,21 @@ const jobRoleResponse: JobRoleResponse = {
     capabilityName: "healthcare",
     formattedClosingDate: new Date('2024-12-31T23:59:59')
 }
+
+//////////////////////////////////////////////////////////////
+
+const jobRoleDetailResponse: JobRoleDetailResponse = {
+    roleName: "Test Engineer",
+    description: "Test description",
+    responsibilities: "Test responsibilities",
+    specification: "Test specification",
+    location: "Test location", 
+    capabilityName: "Test capabilityName",
+    bandValue: "Test bandValue",
+    formattedClosingDate: new Date('2024-12-31T23:59:59.000Z')
+}
+
+//////////////////////////////////////////////////////////////      ^^^^
 
 describe('JobRoleController', function () {
     afterEach(() => {
@@ -48,4 +64,38 @@ describe('JobRoleController', function () {
         expect(res.locals.errormessage).to.equal(errorMessage);
       });
     });
+
+
+    describe('getSingleJobRole', function () {
+        it('should render view with single Job Role Detail page when Job Role Details returned', async () => {
+            const singleJobRoleDetails = jobRoleDetailResponse;
+
+            sinon.stub(JobRoleService, 'getJobRoleByID').resolves(singleJobRoleDetails);
+
+            const req = { params: { id: '1' } };
+            const res = { render: sinon.spy() };
+
+            await JobRoleController.getSingleJobRole(req as any, res as any);
+
+            expect(res.render.calledOnce).to.be.true;
+            expect(res.render.calledWith('jobRoleDetail.html', { jobRole: singleJobRoleDetails })).to.be.true;
+        });
+
+    });
+
+    it('should render view with error message when error thrown', async () => {
+        const errorMessage: string = 'Error message';
+        sinon.stub(JobRoleService, 'getJobRoleByID').rejects(new Error(errorMessage));
+
+        const req = { params: { id: '1' } };
+        const res = { render: sinon.spy(), locals: { errormessage: '' } };
+
+        await JobRoleController.getSingleJobRole(req as any, res as any);
+
+        expect(res.render.calledOnce).to.be.true;
+        expect(res.render.calledWith('jobRoleDetail.html')).to.be.true;
+        expect(res.locals.errormessage).to.equal(errorMessage);
+      });
+
+
 });
