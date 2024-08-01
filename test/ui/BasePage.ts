43937a03e7f73b530a508b89874709d8db009832
jackknowles <jack.knowles@kainos.com>
2024-08-01 09:36:06 +0100

    Removing superfluous methods from the BasePage

--- test/ui/BasePage.ts
+++ test/ui/BasePage.ts
@@ -12,26 +12,24 @@ export default class BasePage {
     await this.driver.get(url);
   }
 
-  //probably can remove these when we have all ids in
-  async enterTextByCss(css: string, text: string) {
-    await this.driver.findElement(By.css(css)).sendKeys(text);
-  }
-
   async enterTextById(id: string, text: string) {
     await this.driver.findElement(By.id(id)).sendKeys(text);
+  }
+
+  async enterTextByCss(css: string, text: string) {
+    await this.driver.findElement(By.css(css)).sendKeys(text);
   }
 
   async clickById(id: string) {
     await this.driver.findElement(By.id(id)).click();
   }
 
-  //probably can remove these when we have all ids in
-  async waitForElementByCss(css: string, timeout: number = 10000) {
-    await this.driver.wait(until.elementLocated(By.css(css)), timeout);
-  }
-
   async waitForElementById(id: string, timeout: number = 10000) {
     await this.driver.wait(until.elementLocated(By.id(id)), timeout);
+  }
+
+  async waitForElementByCss(css: string, timeout: number = 10000) {
+    await this.driver.wait(until.elementLocated(By.css(css)), timeout);
   }
 
   async findElementById(id: string) {
