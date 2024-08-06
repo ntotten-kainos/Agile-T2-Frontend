cfdde17205c42ebeb9d5609f3475e88dd7388822
Jamie McConnell <jamie.mcconnell@kainos.com>
2024-08-06 12:00:58 +0100

    Removed unused .png file

--- src/controllers/JobRoleController.ts
+++ src/controllers/JobRoleController.ts
@@ -8,7 +8,6 @@ export const getAllJobRoles = async (req: express.Request, res: express.Response
         res.locals.errormessage = error.message;
         res.render('jobRoles');
     }
-
 };
 
 export const getSingleJobRole = async (req: express.Request, res: express.Response): Promise<void> => {
